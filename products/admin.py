f4d8abb021b64ffae8bf533e9f21771df5d73e07
dirghayum <dirghayumainali@gmail.com>
2023-01-25 13:44:57 -0700

    Admin panel template updated

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Offer
 
-# Register your models here.
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock' )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
+
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+
 
 def index (request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
 
 
 def newProd(request):
